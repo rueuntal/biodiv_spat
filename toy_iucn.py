30f8f4d1e504d7b4ca1692ad2a518a19812fb904
Xiao Xiao <xiao@weecology.org>
2015-02-12 22:31:50 -0500

    Module with functions to analyze IUCN shapefiles with range maps and convert to richness raster.

---
+++ toy_iucn.py
@@ -0,0 +1,120 @@
+from __future__ import division
+import fiona
+import numpy as np
+from osgeo import ogr, osr, gdal
+import psycopg2
+
+def reproj(in_geom, in_proj4 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs', \
+           out_proj4 = '+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs'):
+    """Function to reproject geometries (defined by WKT). 
+    
+    Default input is unprojected WGS84 (EPSG 4326), default output is Behrmann equal area.
+    
+    """
+    in_geom_ogr = ogr.CreateGeometryFromWkt(in_geom)
+    in_proj = osr.SpatialReference()
+    in_proj.ImportFromProj4(in_proj4)
+    out_proj = osr.SpatialReference()
+    out_proj.ImportFromProj4(out_proj4)
+    transform = osr.CoordinateTransformation(in_proj, out_proj)
+    in_geom_ogr.Transform(transform)
+    return in_geom_ogr.ExportToWkt()
+
+def proj_extent(proj_name):
+    """Give the extend (xmin, xmax, ymin, ymax) of a projection for raster. Only Behrmann is available now."""
+    if proj_name == 'behrmann':
+        # Obtained from http://tiles.arcgis.com/tiles/BG6nSlhZSAWtExvp/arcgis/rest/services/coordsys_Behrmann/MapServer?f=pjson
+        xmin = -1.73675293395E7
+        ymin = -7342230.1365
+        xmax = 1.73675293395E7
+        ymax = 7342230.136500001
+    return [xmin, xmax, ymin, ymax]
+
+def create_array_for_raster(extent, geom = None, no_value = 0, pixel_size = 100000):
+    """Createa an array that can be manipulated and/or later converted to raster.
+    
+    Input:
+    extend: a list in the form of [xmin, xmax, ymin, ymax]. Can be self-defined, or take world extend from proj_extend() for a projection.
+    geom: Default is empty, leading to an array filled with non-values. Or could be a multipolygon (WKT), leading to 1's on the landscape.
+    no_value: value to fill the grid with no data. Default is 0.
+    pixel_size: size of each pixel. Default is 100km. 
+    
+    """
+    xmin, xmax, ymin, ymax = extent
+    x_res = int((xmax - xmin) / pixel_size)
+    y_res = int((ymax - ymin) / pixel_size)
+    target_ds = gdal.GetDriverByName('MEM').Create('', x_res, y_res, gdal.GDT_Byte)
+    target_ds.SetGeoTransform((xmin, pixel_size, 0, ymax, 0, -pixel_size))
+    band = target_ds.GetRasterBand(1)
+    band.SetNoDataValue(no_value)
+    if geom: 
+        # Convert geom to layer
+        virtual_shp = ogr.GetDriverByName('ESRI Shapefile').CreateDataSource('/vsimem/virtual.shp')
+        virtual_layer = virtual_shp.CreateLayer('sp_range', geom_type = ogr.wkbPolygon)
+        idField = ogr.FieldDefn('id', ogr.OFTInteger)
+        virtual_layer.CreateField(idField)
+        featureDefn = virtual_layer.GetLayerDefn()
+        feature = ogr.Feature(featureDefn)
+        feature.SetGeometry(ogr.CreateGeometryFromWkt(geom))
+        feature.SetField('id', 1)
+        virtual_layer.CreateFeature(feature)
+        # This currently throws a warning about missing spatial ref
+        gdal.RasterizeLayer(target_ds, [1], virtual_layer, None, None, [1],  ['ALL_TOUCHED=TRUE'])
+    return band.ReadAsArray()
+
+def convert_array_to_raster(array, rasterOrigin, out_file, pixel_size, \
+                            out_proj4 = '+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs'):
+    """Convert an array to raster."""
+    cols = array.shape[1]
+    rows = array.shape[0]
+    xmin, ymax = rasterOrigin
+    outRaster = gdal.GetDriverByName('GTiff').Create(out_file, cols, rows, 1, gdal.GDT_Float32)
+    outRaster.SetGeoTransform((xmin, pixel_size, 0, ymax, 0, -pixel_size))
+    outRasterSRS = osr.SpatialReference()
+    outRasterSRS.ImportFromProj4(out_proj4)
+    outRaster.SetProjection(outRasterSRS.ExportToWkt())    
+    outband = outRaster.GetRasterBand(1)
+    outband.WriteArray(array)
+    outband.SetNoDataValue(0)
+    outRaster.FlushCache()
+    outRaster = None
+    return None
+    
+def richness_to_raster_by_family(postgis_cur, table_name, out_dir, pixel_size = 100000):
+    """Convert an IUCN shapefile with range maps of a taxon into rasters of richness, one for each family, 
+    
+    assuming that the shapefile has already been loaded into a PostGIS database.
+    Inputs:
+    postgis_cur - cursor pointing to the PostGIS database
+    table_name - name of the table in the PostGIS databse
+    out_dir - output directory for the resulting raster
+    pixel_size - grain size of raster, default is 100km * 100km
+    
+    """
+    xmin, xmax, ymin, ymax = proj_extent('behrmann')
+    family_list_exec = 'SELECT DISTINCT family_nam FROM ' + table_name
+    postgis_cur.execute(family_list_exec)
+    family_list = [x[0] for x in postgis_cur.fetchall()]
+    
+    for family in family_list:
+        sp_list_exec = "SELECT DISTINCT binomial FROM " + table_name + " WHERE family_nam='" + family + "'"
+        postgis_cur.execute(sp_list_exec)
+        sp_list = [x[0] for x in postgis_cur.fetchall()]
+        family_landscape = create_array_for_raster(proj_extent('behrmann'), pixel_size = pixel_size)
+        # Create empty array 
+        for sp in sp_list:
+            sp_geom_exec = "SELECT ST_AsText(geom) FROM " + table_name + " WHERE binomial='" + sp + "'"
+            postgis_cur.execute(sp_geom_exec)
+            sp_geom = [ogr.CreateGeometryFromWkt(x[0]) for x in postgis_cur.fetchall()]
+            union = sp_geom[0]
+            if len(sp_geom) > 1: 
+                for geom in sp_geom: union = union.Union(geom) # This is the range of a species
+            # Reproject geom into Behrmann
+            wkt_reproj = reproj(union.ExportToWkt())
+            # Convert species range to raster array
+            sp_landscape = create_array_for_raster(proj_extent('behrmann'), geom = wkt_reproj, pixel_size = pixel_size)
+            family_landscape += sp_landscape
+        out_file = out_dir + '/' + table_name + '_' + family + '_' + str(int(pixel_size)) + '.tif'
+        convert_array_to_raster(family_landscape, [xmin, ymax], out_file, pixel_size)
+    return None
+

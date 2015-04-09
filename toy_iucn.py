6d61380e8841344b82e5a6990f79320cc8180c47
Xiao Xiao <xiao@weecology.org>
2015-04-08 21:43:01 -0400

    Clean code & remove unused functions. May contain bugs.

--- toy_iucn.py
+++ toy_iucn.py
@@ -24,6 +24,20 @@ def import_shapefile(in_dir):
     for feature in layer:
         geom_list.append(feature.GetGeometryRef().ExportToWkt())
     return geom_list
+
+def write_raster_to_file(out_dir, wide, high, geotrans, proj_wkt, nodata = 0, dtype = gdal.GDT_Float32):    
+    """Create an empty raster at a specified path (in memory if path is None), define geotransform, projection, nodata etc. 
+    
+    and return the object (filled with no data).
+    
+    """
+    if out_dir: outRaster = gdal.GetDriverByName('GTiff').Create(out_dir, wide, high, 1, dtype)
+    else: outRaster = gdal.GetDriverByName('MEM').Create('', wide, high, 1, dtype)
+    if geotrans: outRaster.SetGeoTransform(geotrans)
+    if proj_wkt: outRaster.SetProjection(proj_wkt)
+    outRaster.GetRasterBand(1).SetNoDataValue(nodata)
+    outRaster.GetRasterBand(1).Fill(nodata)
+    return outRaster    
     
 def reproj(in_proj4 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs', \
            out_proj4 = '+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs'):
@@ -86,7 +100,8 @@ def create_array_for_raster(extent, geom = None, no_value = 0, pixel_size = 1000
     
     Input:
     extend: a list in the form of [xmin, xmax, ymin, ymax]. Can be self-defined, or take world extend from proj_extend() for a projection.
-    geom: Default is empty, leading to an array filled with non-values. Or could be a multipolygon (WKT), leading to 1's on the landscape.
+    geom: Default is empty, leading to an array filled with non-values. Or could be a multipolygon (WKT) in the desired projection,
+        leading to 1's on the landscape.
     no_value: value to fill the grid with no data. Default is 0.
     pixel_size: size of each pixel. Default is 100km. 
     
@@ -94,20 +109,15 @@ def create_array_for_raster(extent, geom = None, no_value = 0, pixel_size = 1000
     xmin, xmax, ymin, ymax = extent
     x_res = int((xmax - xmin) / pixel_size)
     y_res = int((ymax - ymin) / pixel_size)
-    target_ds = gdal.GetDriverByName('MEM').Create('', x_res, y_res, gdal.GDT_Float32)
-    target_ds.SetGeoTransform((xmin, pixel_size, 0, ymax, 0, -pixel_size))
-    band = target_ds.GetRasterBand(1)
-    band.SetNoDataValue(no_value)
+    
+    geotrans = (xmin, pixel_size, 0, ymax, 0, -pixel_size)
+    target_ds = write_raster_to_file(None, x_res, y_res, geotrans, None, nodata = no_value, dtype = gdal.GDT_Int32)
     if geom: 
         # Convert geom to layer
         virtual_shp = ogr.GetDriverByName('ESRI Shapefile').CreateDataSource('/vsimem/virtual.shp')
         virtual_layer = virtual_shp.CreateLayer('sp_range', geom_type = ogr.wkbPolygon)
-        idField = ogr.FieldDefn('id', ogr.OFTInteger)
-        virtual_layer.CreateField(idField)
-        featureDefn = virtual_layer.GetLayerDefn()
-        feature = ogr.Feature(featureDefn)
+        feature = ogr.Feature(virtual_layer.GetLayerDefn())
         feature.SetGeometry(ogr.CreateGeometryFromWkt(geom))
-        feature.SetField('id', 1)
         virtual_layer.CreateFeature(feature)
         # This currently throws a warning about missing spatial ref
         gdal.RasterizeLayer(target_ds, [1], virtual_layer, None, None, [1],  ['ALL_TOUCHED=TRUE'])
@@ -119,14 +129,14 @@ def convert_array_to_raster(array, rasterOrigin, out_file, pixel_size, no_value 
     cols = array.shape[1]
     rows = array.shape[0]
     xmin, ymax = rasterOrigin
-    outRaster = gdal.GetDriverByName('GTiff').Create(out_file, cols, rows, 1, gdal.GDT_Float32)
-    outRaster.SetGeoTransform((xmin, pixel_size, 0, ymax, 0, -pixel_size))
+    geotrans = (xmin, pixel_size, 0, ymax, 0, -pixel_size)
     outRasterSRS = osr.SpatialReference()
     outRasterSRS.ImportFromProj4(out_proj4)
-    outRaster.SetProjection(outRasterSRS.ExportToWkt())    
+    proj_wkt = outRasterSRS.ExportToWkt()
+    
+    outRaster = write_raster_to_file(out_file, cols, rows, geotrans, proj_wkt, nodata = no_value)
     outband = outRaster.GetRasterBand(1)
     outband.WriteArray(array)
-    outband.SetNoDataValue(no_value)
     outRaster.FlushCache()
     outRaster = None
     return None
@@ -192,7 +202,7 @@ def richness_to_raster_birds(folder, out_dir, pixel_size = 100000, remove_sp_lis
     return None
  
 def richness_to_raster_ver2(sp_array, out_dir, out_name, pixel_size = 100000, remove_sp_list = []):  
-    """Another way to convert ricness to raster which may be faster."""
+    """Another way to convert richess to raster using existing array of species list, which is much faster."""
     xmin, xmax, ymin, ymax = proj_extent('behrmann')
     table_richness = np.zeros(shape = sp_array.shape)
     for j in range(len(sp_array)):
@@ -204,38 +214,38 @@ def richness_to_raster_ver2(sp_array, out_dir, out_name, pixel_size = 100000, re
     convert_array_to_raster(table_richness, [xmin, ymax], out_file, pixel_size)
     return None
 
-def richness_to_raster_by_family(postgis_cur, table_name, out_dir, pixel_size = 100000):
-    """Convert an IUCN shapefile with range maps of a taxon into rasters of richness, one for each family, 
+#def richness_to_raster_by_family(postgis_cur, table_name, out_dir, pixel_size = 100000):
+    #"""Convert an IUCN shapefile with range maps of a taxon into rasters of richness, one for each family, 
     
-    assuming that the shapefile has already been loaded into a PostGIS database.
-    Inputs:
-    postgis_cur - cursor pointing to the PostGIS database
-    table_name - name of the table in the PostGIS databse
-    out_dir - output directory for the resulting raster
-    pixel_size - grain size of raster, default is 100km * 100km
+    #assuming that the shapefile has already been loaded into a PostGIS database.
+    #Inputs:
+    #postgis_cur - cursor pointing to the PostGIS database
+    #table_name - name of the table in the PostGIS databse
+    #out_dir - output directory for the resulting raster
+    #pixel_size - grain size of raster, default is 100km * 100km
     
-    """
-    xmin, xmax, ymin, ymax = proj_extent('behrmann')
-    family_list_exec = 'SELECT DISTINCT family_nam FROM ' + table_name
-    postgis_cur.execute(family_list_exec)
-    family_list = [x[0] for x in postgis_cur.fetchall()]
+    #"""
+    #xmin, xmax, ymin, ymax = proj_extent('behrmann')
+    #family_list_exec = 'SELECT DISTINCT family_nam FROM ' + table_name
+    #postgis_cur.execute(family_list_exec)
+    #family_list = [x[0] for x in postgis_cur.fetchall()]
     
-    for family in family_list:
-        out_file = out_dir + '/' + table_name + '_' + family + '_' + str(int(pixel_size)) + '.tif'
-        # Check to make sure file doesn't exist already - useful to resume from unexpected breaks
-        if not os.path.exists(out_file):
-            sp_list_exec = "SELECT DISTINCT binomial FROM " + table_name + " WHERE family_nam='" + family + "'"
-            postgis_cur.execute(sp_list_exec)
-            sp_list = [x[0] for x in postgis_cur.fetchall()]
-            family_landscape = create_array_for_raster(proj_extent('behrmann'), pixel_size = pixel_size)
-            # Create empty array 
-            for sp in sp_list:
-                wkt_reproj = sp_reproj(postgis_cur, table_name, sp)
-                # Convert species range to raster array
-                sp_landscape = create_array_for_raster(proj_extent('behrmann'), geom = wkt_reproj, pixel_size = pixel_size)        
-                family_landscape += sp_landscape
-            convert_array_to_raster(family_landscape, [xmin, ymax], out_file, pixel_size)
-    return None
+    #for family in family_list:
+        #out_file = out_dir + '/' + table_name + '_' + family + '_' + str(int(pixel_size)) + '.tif'
+        ## Check to make sure file doesn't exist already - useful to resume from unexpected breaks
+        #if not os.path.exists(out_file):
+            #sp_list_exec = "SELECT DISTINCT binomial FROM " + table_name + " WHERE family_nam='" + family + "'"
+            #postgis_cur.execute(sp_list_exec)
+            #sp_list = [x[0] for x in postgis_cur.fetchall()]
+            #family_landscape = create_array_for_raster(proj_extent('behrmann'), pixel_size = pixel_size)
+            ## Create empty array 
+            #for sp in sp_list:
+                #wkt_reproj = sp_reproj(postgis_cur, table_name, sp)
+                ## Convert species range to raster array
+                #sp_landscape = create_array_for_raster(proj_extent('behrmann'), geom = wkt_reproj, pixel_size = pixel_size)        
+                #family_landscape += sp_landscape
+            #convert_array_to_raster(family_landscape, [xmin, ymax], out_file, pixel_size)
+    #return None
 
 def create_sp_range_dic(postgis_cur, table_name, out_file_name):
     """Obtain the range size of species (in m^2), put in a dictionary, and save to file. 
@@ -273,88 +283,6 @@ def create_sp_range_dic_bird(folder, out_file_name):
     out_file.close()
     return None        
 
-def create_marine_sp_list(postgis_cur, table_name, ocean_file_dir, out_file_name, threshold = 0.5):
-    """Identify species with a significant proportion of their ranges (above a designated threshold)
-    
-    in the ocean and save the list.
-    
-    """
-    sp_list_exec = 'SELECT DISTINCT binomial FROM ' + table_name
-    postgis_cur.execute(sp_list_exec)
-    sp_list = [x[0] for x in postgis_cur.fetchall()] 
-    
-    # Reproject the ocean polygons
-    ocean_geom_list = import_shapefile(ocean_file_dir)
-    ocean_geom = [shapely.wkt.loads(x) for x in ocean_geom_list]
-    try:
-        ocean_shape = shapely.ops.cascaded_union(ocean_geom)
-    except: 
-        ocean_geom = [x.buffer(0) for x in ocean_geom] 
-        ocean_shape = shapely.ops.cascaded_union(ocean_geom)
-    ocean_reproj_wkt = reproj_geom(shapely.wkt.dumps(ocean_shape))
-    ocean_reproj = shapely.wkt.loads(ocean_reproj_wkt)    
-    # Convert ocean_reproj into array for a crude comparison first
-    ocean_array = create_array_for_raster(proj_extent('behrmann'), ocean_reproj_wkt)
-
-    marine_sp_list = []
-    for sp in sp_list:
-        wkt_reproj = sp_reproj(postgis_cur, table_name, sp)
-        sp_array = create_array_for_raster(proj_extent('behrmann'), wkt_reproj)        
-        # 1. A crude and fast assessment by overlaying the array of ceans and the array of species
-        sp_ocean_array = sp_array + ocean_array
-        # This is the number of overlapping grids between the species range and the ocean
-        num_2 = len(np.where(sp_ocean_array == 2)[0])
-        if num_2 >= np.sum(sp_array) * threshold: # If the number of overlapping grids is larger than a proportion of species range
-            # 2. A more accurate calculation using polygons instead of rasters
-            sp_range_shape = shapely.wkt.loads(wkt_reproj)
-            try: intersect = sp_range_shape.intersection(ocean_reproj)
-            except: 
-                sp_range_shape = sp_range_shape.buffer(0)
-                intersect = sp_range_shape.intersection(ocean_reproj)
-            if intersect.area > sp_range_shape.area * threshold: 
-                marine_sp_list.append(sp)
-    
-    out_file = open(out_file_name, 'wb')
-    cPickle.dump(marine_sp_list, out_file, protocol = 2)
-    out_file.close()
-    return None        
-
-def create_marine_sp_list_birds(folder, ocean_file_dir, out_file_name, threshold = 0.1):
-    """create_marine_sp_list() for birds. """
-    # Reproject the ocean polygons
-    ocean_geom_list = import_shapefile(ocean_file_dir)
-    ocean_geom = [shapely.wkt.loads(x) for x in ocean_geom_list]
-    try:
-        ocean_shape = shapely.ops.cascaded_union(ocean_geom)
-    except: 
-        ocean_geom = [x.buffer(0) for x in ocean_geom] 
-        ocean_shape = shapely.ops.cascaded_union(ocean_geom)
-    ocean_reproj_wkt = reproj_geom(shapely.wkt.dumps(ocean_shape))
-    ocean_reproj = shapely.wkt.loads(ocean_reproj_wkt)    
-    # Convert ocean_reproj into array for a crude comparison first
-    ocean_array = create_array_for_raster(proj_extent('behrmann'), ocean_reproj_wkt)
-    
-    marine_sp_list = []
-    for file in os.listdir(folder):
-        if file.endswith('.shp'):
-            sp_name, wkt_reproj = sp_reproj_birds(folder, file)            
-            sp_array = create_array_for_raster(proj_extent('behrmann'), wkt_reproj)
-            sp_ocean_array = sp_array + ocean_array
-            num_2 = len(np.where(sp_ocean_array == 2)[0])
-            if num_2 >= np.sum(sp_array) * threshold: 
-                sp_range_shape = shapely.wkt.loads(wkt_reproj)
-                try: intersect = sp_range_shape.intersection(ocean_reproj)
-                except: 
-                    sp_range_shape = sp_range_shape.buffer(0)
-                    intersect = sp_range_shape.intersection(ocean_reproj)
-                if intersect.area > sp_range_shape.area * threshold:
-                    marine_sp_list.append(sp_name)
-        
-    out_file = open(out_file_name, 'wb')
-    cPickle.dump(marine_sp_list, out_file, protocol = 2)
-    out_file.close()
-    return None        
-            
 def create_array_sp_list(postgis_cur, table_name, out_file_name, pixel_size = 100000):
     """Create an array with species list in each grid covering the globe."""
     xmin, xmax, ymin, ymax = proj_extent('behrmann')
@@ -442,7 +370,7 @@ def compare_range_size_dists(sp_list_array, range_size_dic, out_dir, out_name, N
     The output file is written to the designated output directory, with the same out_name + "_" + metric + ".pck".
     
     """
-    array_out_list = [np.empty([len(sp_list_array), len(sp_list_array[0])], dtype = float) for i in range(len(metrics))]
+    array_out_list = [np.empty([len(sp_list_array), len(sp_list_array[0])], dtype = float) for k in range(len(metrics))]
     # Remove marine species from the dictionary of range sizes  
     for marine_sp in marine_list: del range_size_dic[marine_sp]
     range_size_list = range_size_dic.values()  
@@ -473,44 +401,10 @@ def compare_range_size_dists(sp_list_array, range_size_dic, out_dir, out_name, N
     
     # Save to file
     for k, metric in enumerate(metrics):
-        out_file_name = out_dir + '/' + out_name + '_' + metric + '.pck'
+        out_file_name = out_dir + '/' + out_name + '_' + metric + '.pkl'
         out_file = open(out_file_name, 'wb')
         cPickle.dump(array_out_list[k], out_file, protocol = 2)
         out_file.close()
-    return None
-
-def reproj_raster(in_dir, out_dir, pixel_size = 100000, alg = gdal.GRA_Bilinear, \
-                  in_proj4 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs', \
-                  out_proj4 = '+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs'):
-    """Resample and reproject a raster layer. Defaut is from WGS 84 to Behrmann Equal Area. 
-    
-    This is based on the example from http://jgomezdans.github.io/gdal_notes/reprojection.html. 
-    
-    """
-    in_proj = osr.SpatialReference()
-    in_proj.ImportFromProj4(in_proj4)
-    out_proj = osr.SpatialReference()
-    out_proj.ImportFromProj4(out_proj4)
-    transform = reproj(in_proj4 = in_proj4, out_proj4 = out_proj4)
-    in_file = gdal.Open(in_dir)
-    in_nodata = in_file.GetRasterBand(1).GetNoDataValue()
-    
-    geo_t = in_file.GetGeoTransform()
-    x_size = in_file.RasterXSize
-    y_size = in_file.RasterYSize
-    (ulx, uly, ulz) = transform.TransformPoint(geo_t[0], geo_t[3])
-    (lrx, lry, lrz) = transform.TransformPoint(geo_t[0] + geo_t[1] * x_size, \
-                                               geo_t[3] + geo_t[5] * y_size)
-    outRaster = gdal.GetDriverByName('GTiff').\
-        Create(out_dir, int((lrx - ulx) / pixel_size), int((uly - lry) / pixel_size), 1, gdal.GDT_Float32)
-    new_geo = (ulx, pixel_size, geo_t[2], uly, geo_t[4], -pixel_size)
-    outRaster.SetGeoTransform(new_geo)
-    outRaster.SetProjection(out_proj.ExportToWkt())
-    outRaster.GetRasterBand(1).SetNoDataValue(in_nodata)
-    outRaster.GetRasterBand(1).Fill(in_nodata)
-    res = gdal.ReprojectImage(in_file, outRaster, in_proj.ExportToWkt(), out_proj.ExportToWkt(), alg)
-    outRaster.FlushCache()
-    outRaster = None    
     return None
 
 def reproj_raster_to_match(in_dir, out_dir, match_dir, alg = gdal.GRA_Bilinear, \
@@ -525,7 +419,6 @@ def reproj_raster_to_match(in_dir, out_dir, match_dir, alg = gdal.GRA_Bilinear, 
         in_proj = osr.SpatialReference()
         in_proj.ImportFromProj4(in_proj4)
         in_proj_wkt = in_proj.ExportToWkt()
-    in_file_geotrans = in_file.GetGeoTransform()
     in_nodata = in_file.GetRasterBand(1).GetNoDataValue()
   
     match_file = gdal.Open(match_dir)
@@ -534,11 +427,7 @@ def reproj_raster_to_match(in_dir, out_dir, match_dir, alg = gdal.GRA_Bilinear, 
     wide = match_file.RasterXSize
     high = match_file.RasterYSize
     
-    outRaster = gdal.GetDriverByName('GTiff').Create(out_dir, wide, high, 1, gdal.GDT_Float32)
-    outRaster.SetGeoTransform(match_geotrans)
-    outRaster.SetProjection(match_proj_wkt)
-    outRaster.GetRasterBand(1).SetNoDataValue(in_nodata)
-    outRaster.GetRasterBand(1).Fill(in_nodata)
+    outRaster = write_raster_to_file(out_dir, wide, high, match_geotrans, match_proj_wkt, nodata = in_nodata)
     res = gdal.ReprojectImage(in_file, outRaster, in_proj_wkt, match_proj_wkt, alg)
     outRaster.FlushCache()
     outRaster = None    

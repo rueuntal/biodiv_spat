6f4a2a4538507750d3468e7909cabed859d596cc
Xiao Xiao <xiao@weecology.org>
2015-05-06 11:53:03 -0400

    Add functions to convert 2-d indices from/to 1-d. (untested)

--- range_size_diversity_sim.py
+++ range_size_diversity_sim.py
@@ -16,16 +16,64 @@ def global_range_size(mu, sigma, S, max_size):
         sizes = np.append(sizes, additional_ranges)
     return sizes
 
-def sim_range_size_landscape(width, height, mu, sigma, S, Nsim):
+def convert_2D_to_1D(i, j, width, height):
+    """Convert [i, j] in a grid with known width and height to 1-D index."""
+    return j * width + i
+
+def convert_1D_to_2D(index, width, height):
+    """Convert a 1-D index back to [i, j] in grid."""
+    j = int(np.floor(index / width))
+    i = index - j * width
+    return i, j
+
+def ind_range_generator(width, height, size, continuous = False, env = False, env_landscape = None, r = None):
+    """Generate the spatial range of one species on the landscale.
+    
+    Inputs:
+    width, height - dimension of the landscape.
+    size - range size of the focal species.
+    continuous - whether the range of each species is continuous or scattered. 
+    env - whether the range correlates with an environmental variable.
+    env_landscape - the landscape of the environmental variable. Only needed when env = True.
+    r - correlation coefficient between (transformed) probability of species presence and
+        environmental variable. Only needed when env = True.
+    
+    Output:
+    spatial_range - an array of shape (height, width) where True is for presence and False is for absence.
+    """
+    np.random.seed()
+    spatial_range = np.empty([height, width], dtype = bool)
+    spatial_range.fill(False)
+    size_grid = int(np.ceil(size))
+    if env: # Convert the environment landscape to probability landscale
+        env_flat = np.array([env_landscape[j][i] for j in range(height) for i in range(width)])
+        noise_term = scipy.stats.norm.rvs(size = len(env_flat))
+        noise_term = (noise_term - np.mean(noise_term)) / np.std(noise_term, ddof = 1)
+        a = r / np.sqrt(1 - r **2)
+        env_flat_standardize = (env_flat - np.mean(env_flat)) / np.std(env_flat, ddof = 1)
+        p_transform = a * env_flat_standardize + noise_term
+        p_raw = 1 / (1 + np.exp(-p_transform)) # Use logistic function to convert transformed env to p
+        p = p_raw / np.sum(p_raw)
+    else: p = None
+    if not continuous:
+        loc_sp = choice(range(width * height), size = size_grid, replace = False, p = p)
+        
+def sim_range_size_landscape(width, height, mu, sigma, S, continuous = False):
     """Simulate species range sizes on a landscape and explore their correlations with overall diversity.
     
     Input:
-    width, height - dimension of the grid.
+    width, height - dimension of the landscape (grid cells).
     mu, sigma - parameters for the Poisson lognormal distribution from which the global range size distribution is simulated.
     S - total number of species
+    continuous - whether the range of each species is continuous or scattered. If True, the range is in one polygon. If False,
+        each cell is randomly assigned.
     
     Output: 
     r2_quartile -  a list with 4 R^2 values showing correlation between richness in each range size quartile and overall 
         richness, from the smallest species to the largest species.
+    r2_low - a list of length S, with R^2 between total diversity and cumulative diversity from the smallest-ranged species to
+        the largest-ranged species.
+    r2_high - a list of length S, with R^2 between total diversity and cumulative diversity from the largest-ranged species to
+        the smallest-ranged species.
     
     """

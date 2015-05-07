173f6de026f7dd18c268f3f721820673a2318026
Xiao Xiao <xiao@weecology.org>
2015-05-06 23:05:06 -0400

    Add function to generate continous species ranges.

--- range_size_diversity_sim.py
+++ range_size_diversity_sim.py
@@ -23,9 +23,53 @@ def convert_2D_to_1D(i, j, width):
 def convert_1D_to_2D(index, width):
     """Convert a 1-D index back to [i, j] in grid."""
     j = int(np.floor(index / width))
-    i = index - j * width
+    i = int(index - j * width)
     return i, j
 
+def new_edge_cells(i, j, width, height):
+    """Given the position of the focal cell [i, j], return the location of its 4 surrouding cells, 
+    
+    taking into account grid boundaries.
+    
+    """
+    edge_list = []
+    if i >= 1: edge_list.append([i - 1, j])
+    if i <= width - 2: edge_list.append([i + 1, j])
+    if j >= 1: edge_list.append([i, j - 1])
+    if j <= height - 2: edge_list.append([i, j + 1])
+    return edge_list
+
+def ind_range_continuous(width, height, Nsize, env_p = None):
+    """Subfunction to generate a continuous species range.
+    
+    Inputs:
+    width, height - dimension of the landscape.
+    Nsize - size of the focal range, in number of grid cells.
+    env_p - 1-D list of probability of occupancy. If None, all cells are equally likely.
+    
+    """
+    np.random.seed()
+    range_set = set([])
+    edge_set = set([])
+    loc = np.random.choice(range(width * height), size = 1, p = env_p)
+    while len(range_set) < Nsize:
+        range_set.update(loc)
+        i, j = convert_1D_to_2D(loc, width)
+        new_edges = new_edge_cells(i, j, width, height)
+        new_edges_1D = [convert_2D_to_1D(cell[0], cell[1], width) for cell in new_edges]
+        new_edges_1D = [cell for cell in new_edges_1D if cell not in range_set]
+        edge_set.update(new_edges_1D)
+        # Obtain p for edge cells
+        edge_list = list(edge_set)
+        if env_p: 
+            p_edge = [env_p[index] for index in edge_list]
+            p_edge = np.array(p_edge)  / np.sum(p_edge)
+        else: p_edge = None
+        loc = np.random.choice(edge_list, size = 1, p = p_edge)
+        edge_set.remove(int(loc))
+    
+    return np.array(list(range_set))
+        
 def ind_range_generator(width, height, size, continuous = False, env = False, env_landscape = None, r = None):
     """Generate the spatial range of one species on the landscale.
     
@@ -56,11 +100,12 @@ def ind_range_generator(width, height, size, continuous = False, env = False, en
         p = p_raw / np.sum(p_raw)
     else: p = None
     if not continuous:
-        loc_sp = choice(range(width * height), size = size_grid, replace = False, p = p)
-        for loc in loc_sp:
-            i, j = convert_1D_to_2D(loc, width)
-            spatial_range[j][i] == True
-    #else: 
+        loc_sp = np.random.choice(range(width * height), size = size_grid, replace = False, p = p)
+    else:
+        loc_sp = ind_range_continuous(width, height, size_grid, env_p = p)
+    for loc in loc_sp:
+        i, j = convert_1D_to_2D(loc, width)
+        spatial_range[j][i] == True
     return spatial_range
 
 def sim_range_size_landscape(width, height, mu, sigma, S, continuous = False):

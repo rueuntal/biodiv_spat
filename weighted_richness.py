f9bcf363557c44ce00cb3c91f0830a4cf37bfe7f
Xiao Xiao <xiao@weecology.org>
2016-01-16 21:35:06 -0500

    Add lines to create rasters for weighted richness.

--- weighted_richness.py
+++ weighted_richness.py
@@ -15,15 +15,30 @@ if __name__ == '__main__':
     con_ranges = psycopg2.connect("dbname = 'IUCN_range_maps' port = 5432 user = 'postgres' password = 'lavender' host = 'localhost'")
     postgis_cur = con_ranges.cursor()
     pixel_size = 100000
-    out_folder_sp_dist = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\sp_dist'
+    out_folder_sp_dist = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\sp_dist\\'
     
     for taxon in taxa:
-        spat.create_array_sp_list(postgis_cur, taxon, out_folder_sp_dist + taxon + '_' + str(pixel_size) + 'pkl', pixel_size = pixel_size)
+        spat.create_array_sp_list(postgis_cur, taxon, out_folder_sp_dist + taxon + '_' + str(pixel_size) + '.pkl', pixel_size = pixel_size)
         spat.create_sp_range_dic(postgis_cur, taxon, out_folder_sp_dist + taxon + '_range_size.pkl')
         
-    birds_folder = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\IUCN_range_maps\\BIRDS'
-    spat.create_array_sp_list_birds(birds_folder, out_folder_sp_dist + 'birds_' + str(pixel_size) + '100000.pkl', pixel_size = pixel_size)
+    birds_folder = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\IUCN_range_maps\\BIRDS\\'
+    spat.create_array_sp_list_birds(birds_folder, out_folder_sp_dist + 'birds_' + str(pixel_size) + '.pkl', pixel_size = pixel_size)
     spat.create_sp_range_dic_bird(birds_folder, out_folder_sp_dist + 'birds_range_size.pkl')    
 
-    # 2. Obtain rasters of environmental variables in same projection & resolution
-    # 
+    # 2. Compute the weighted richness
+    out_folder_weightedS = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\weighted_S\\'
+    for taxon in taxa:
+        array_taxon = spat.import_pickle_file(out_folder_sp_dist + taxon + '_' + str(pixel_size) + '.pkl')
+        range_dic_taxon = spat.import_pickle_file(out_folder_sp_dist + taxon + '_range_size.pkl')
+        for q in np.arange(0, 1.1, 0.1):
+            spat.weighted_richness_to_raster(array_taxon, range_dic_taxon, -q, out_folder_weightedS, taxon)
+     
+    # Birds is a bit complicated b/c of migration
+    
+    # 3. Obtain rasters of environmental variables in same projection & resolution
+    # Variables included: mean annual T, PET, AET, NDVI, altitudinal range, seasonality
+    # mean annual T & seasonality are obtained from Bioclim (BIO1 and BIO4)
+    in_folder_env = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\pred_vars_raw\\'
+    out_folder_env = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\pred_vars\\'
+    in_folder_bioclim = in_folder_env + 'bio_10m_bil\\'
+    

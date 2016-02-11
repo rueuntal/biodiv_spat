f9312a1b20eb47c2c1c6783163f7137b398b37e0
Xiao Xiao <xiao@weecology.org>
2016-02-11 13:39:05 -0500

    Pull inputs to the front to save myself some trouble running the pieces.

--- weighted_richness.py
+++ weighted_richness.py
@@ -19,22 +19,38 @@ with open('C:\\Users\\Xiao\\Documents\\GitHub\\gis_sandbox\\regression.R', 'r') 
     string = f.read()
     
 if __name__ == '__main__':
-    # 1. Map species ranges onto 100km*100km grid cells, and obtain dictionaries of range sizes
-    # Range maps of terrestrial mammals, birds, and amphibians are obtained from IUCN
-    # Projection: Behrmann equal area
+    # Define the variables and inputs
     taxa = ['terrestrial_mammals', 'amphibians']
+    taxa_birds = ['birds_resident', 'birds_breeding', 'birds_wintering']
     con_ranges = psycopg2.connect("dbname = 'IUCN_range_maps' port = 5432 user = 'postgres' password = 'lavender' host = 'localhost'")
     postgis_cur = con_ranges.cursor()
     pixel_size = 100000
-    out_folder_sp_dist = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\sp_dist\\'
+    birds_folder = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\IUCN_range_maps\\BIRDS\\'
+    out_folder_sp_dist = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\sp_dist\\' 
+    out_folder_weightedS = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\weighted_S\\'
+    seabirds_list = np.genfromtxt('C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\seabirds.csv', dtype = None, \
+                                   delimiter = ',', skip_header = 1, names = ['common name', 'binomial'])    
+    out_plot_folder = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\plots\\'
+    in_folder_env = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\pred_vars_raw\\'
+    out_folder_env = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\pred_vars\\'
+    match_file = out_folder_weightedS + taxa[0] + '_weighted_richness_' + str(pixel_size) + '_1.0.tif'
+    in_folder_bioclim = in_folder_env + 'bio_10m_bil\\'
+    in_folder_alt = in_folder_env + 'alt_30s_bil\\'
+    in_folder_pet = in_folder_env + 'PET_he_annual\\pet_he_yr\\'
+    in_folder_aet = in_folder_env + 'AET_YR\\aet_yr\\'
+    in_folder_monthly_ndvi = in_folder_env + 'monthly\\gimms_ndvi_qd_1981-2002\\'
+    out_analysis_folder = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\output\\'
+    out_folder_monthly_ndvi = in_folder_env + 'monthly\\monthly_ndvi\\'
     
+    # 1. Map species ranges onto 100km*100km grid cells, and obtain dictionaries of range sizes
+    # Range maps of terrestrial mammals, birds, and amphibians are obtained from IUCN
+    # Projection: Behrmann equal area    
     for taxon in taxa:
         spat.create_array_sp_list(postgis_cur, taxon, out_folder_sp_dist + taxon + '_' + str(pixel_size) + '.pkl', pixel_size = pixel_size)
         spat.create_sp_range_dic(postgis_cur, taxon, out_folder_sp_dist + taxon + '_range_size.pkl')
-        
-    birds_folder = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\IUCN_range_maps\\BIRDS\\'
-    spat.create_array_sp_list_birds(birds_folder, out_folder_sp_dist + 'birds_' + str(pixel_size) + '.pkl', pixel_size = pixel_size)
-    spat.create_sp_range_dic_bird(birds_folder, out_folder_sp_dist + 'birds_range_size.pkl')    
+            
+    #spat.create_array_sp_list_birds(birds_folder, out_folder_sp_dist + 'birds_' + str(pixel_size) + '.pkl', pixel_size = pixel_size)
+    #spat.create_sp_range_dic_bird(birds_folder, out_folder_sp_dist + 'birds_range_size.pkl')    
     
     # A separate map of birds with only resident species, & dictionary with species ranges for resident species
     spat.create_array_sp_list_birds(birds_folder, out_folder_sp_dist + 'birds_resident_' + str(pixel_size) + '.pkl',
@@ -54,11 +70,7 @@ if __name__ == '__main__':
     # 2. Compute the weighted richness
     # Birds is a bit complicated b/c of migration
     # Separate breeding (breeding + resident) and wintering (non-breeding + resident) communities    
-    out_folder_weightedS = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\weighted_S\\'
-    taxa.extend(['birds_resident', 'birds_breeding', 'birds_wintering'])  
-    seabirds_list = np.genfromtxt('C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\seabirds.csv', dtype = None, \
-                                   delimiter = ',', skip_header = 1, names = ['common name', 'binomial'])    
-    for taxon in taxa:
+    for taxon in taxa + taxa_birds:
         array_taxon = spat.import_pickle_file(out_folder_sp_dist + taxon + '_' + str(pixel_size) + '.pkl')
         range_dic_taxon = spat.import_pickle_file(out_folder_sp_dist + taxon + '_range_size.pkl')
         for q in np.arange(-10, 11, 1)/10: # This is to prevent weird approximation problem at zero
@@ -68,28 +80,21 @@ if __name__ == '__main__':
                 spat.weighted_richness_to_raster(array_taxon, range_dic_taxon, q, out_folder_weightedS, taxon)
          
     # 2'. Compute and plot the correlation between raw richness and weighted richness for each taxon
-    out_plot_folder = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\plots\\'
-    fig_SandS = plt.figure(figsize = (10.5, 4))
-    for i, taxon in enumerate(taxa):
-        ax = plt.subplot(1, 3, i + 1)
+    fig_SandS = plt.figure(figsize = (10.5, 7))
+    for i, taxon in enumerate(taxa + taxa_birds):
+        ax = plt.subplot(2, 3, i + 1)
         spat.plot_r2_weighted_S(out_folder_weightedS, taxon, '_weighted_richness_100000_', ax = ax)
         plt.title(taxon)
-    plt.subplots_adjust(wspace = 0.29)
+    plt.subplots_adjust(wspace = 0.29, hspace = 0.29)
     plt.savefig(out_plot_folder + 'corrS.png', dpi = 600)
         
     # 3. Obtain rasters of environmental variables in same projection & resolution
-    # Variables included: mean annual T, PET, AET, NDVI, altitudinal range, seasonality
-    in_folder_env = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\pred_vars_raw\\'
-    out_folder_env = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\pred_vars\\'
-    match_file = out_folder_weightedS + taxa[0] + '_weighted_richness_' + str(pixel_size) + '_' + str(q) + '.tif'
-    
+    # Variables included: mean annual T, PET, AET, NDVI, altitudinal range, seasonality    
     # mean annual T & seasonality are obtained from WordClim (BIO1 and BIO4)
-    in_folder_bioclim = in_folder_env + 'bio_10m_bil\\'
     spat.reproj_raster_to_match(in_folder_bioclim + 'bio1.bil', out_folder_env + 'mean_annual_T.tif', match_file)
     spat.reproj_raster_to_match(in_folder_bioclim + 'bio4.bil', out_folder_env + 'seasonality.tif', match_file)
     
     # Altitudinal range is also obtained from WorldClim but requires additional processing in R
-    in_folder_alt = in_folder_env + 'alt_30s_bil\\'
     spat.reproj_raster_pixel_size(in_folder_alt + 'alt.bil', out_folder_env + 'alt_behrmann_1000.tif', pixel_size= 1000) 
     # Get min and max in each pixel using R
     r_cmd = "C:\\Program Files\\R\\R-3.1.0\\bin\\x64\\Rscript.exe"
@@ -102,15 +107,12 @@ if __name__ == '__main__':
     spat.get_range_raster(alt_max_dir, alt_min_dir, alt_range_dir)
     
     # PET and AET are obtained from CGIAR-CSI
-    in_folder_pet = in_folder_env + 'PET_he_annual\\pet_he_yr\\'
     spat.reproj_raster_to_match(in_folder_pet + 'w001001.adf', out_folder_env + 'PET.tif', match_file)
-    in_folder_aet = in_folder_env + 'AET_YR\\aet_yr\\'
     spat.reproj_raster_to_match(in_folder_aet + 'w001001.adf', out_folder_env + 'AET.tif', match_file)
     
     # Monthly NDVI is obtained from GIMMS: https://daac.ornl.gov/ISLSCP_II/guides/gimms_ndvi_monthly_xdeg.html
     # Mean annual NDVI is calculated by taking the average across the files
     # Only valid values (between -1 and 1) are included
-    in_folder_monthly_ndvi = in_folder_env + 'monthly\\gimms_ndvi_qd_1981-2002\\'
     spat.obtain_avg_ndvi(in_folder_monthly_ndvi, 1982, 2002, out_folder_env + 'NDVI_WGS84.tif')
     spat.reproj_raster_to_match(out_folder_env + 'NDVI_WGS84.tif', out_folder_env + 'NDVI.tif', match_file)
     
@@ -118,7 +120,6 @@ if __name__ == '__main__':
     # Predictors are divided into three groups: temperature (mean annual T & PET), productivity (AET & NDVI),
     # and Janzen's hypothesis (seasonsality, altitudinal range, and interaction)
     # This section requires the module "rpy2" and calls the R script regression.R for multiple regression.
-    out_analysis_folder = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\output\\'
     multilin = STAP(string, 'multilin')    
     for taxon in taxa:
         for q in np.arange(-10, 11, 1)/10:
@@ -133,7 +134,7 @@ if __name__ == '__main__':
     
     # Plot q versus r-squared for the four models, as well as unique contributions from each set of variables, for each taxon
     results_multilin_dir = out_analysis_folder + 'multilin.txt'
-    for taxon in taxa:
+    for taxon in taxa + ['birds_resident']:
         out_plot_dir = out_plot_folder + taxon + '_r2_multilin.png'
         spat.plot_r2_multilin(results_multilin_dir, taxon, out_plot_dir)
     
@@ -141,4 +142,6 @@ if __name__ == '__main__':
     # Breeding season is defined as the month with the highest monthly average NDVI for each cell
     # Wintering season is defined as 6 months from the breeding season
     # All other variables are selected based on the timing of NDVI
+    # 5a. Obtain average NDVI for each month 
+    spat.obtain_avg_ndvi(in_folder_monthly_ndvi, 1982, 2002, out_folder_monthly_ndvi)
     

e22c330da8bdfdea41085ed91d9650cccd1c4df8
Xiao Xiao <xiao@weecology.org>
2016-02-12 14:16:00 -0500

    Rewrite the function to accommodate breeding and wintering communities.

--- regression.R
+++ regression.R
@@ -13,10 +13,16 @@ library(raster)
 # r^2 for the full, the temperature, the productivity and Janzen's models
 # Unique contribution of temperature, productivity, and Janzen's effect
 # p-value of the seven variables in the full model
-multilin = function(S_dir){
+multilin = function(S_dir, env = 'annual'){
   dat = c(as.matrix(raster(S_dir)))
   pred_dir = 'C:\\Users\\Xiao\\Dropbox\\projects\\range_size_dist\\Janzen\\pred_vars\\'
-  pred_names = c('mean_annual_T', 'PET', 'AET', 'NDVI', 'alt_range', 'seasonality')
+  if (env == 'annual'){
+    pred_names = c('mean_annual_T', 'PET', 'AET', 'NDVI', 'alt_range', 'seasonality')
+  } else if (env == 'breeding'){
+    pred_names = c('mean_T_max', 'PET_max', 'AET_max', 'NDVI_max', 'alt_range', 'seasonality')
+  } else {
+    pred_names = c('mean_T_min', 'PET_min', 'AET_min', 'NDVI_min', 'alt_range', 'seasonality')
+  }
   for (pred in pred_names){
     pred_file = paste(pred_dir, pred, '.tif', sep = '')
     pred_raster = c(as.matrix(raster(pred_file)))
@@ -29,20 +35,20 @@ multilin = function(S_dir){
   dat = dat[index, ]
   
   out = vector(mode = 'numeric', length = 14)
-  full_model = lm(log(S) ~ mean_annual_T + PET + AET + NDVI + alt_range * seasonality, data = dat)
+  full_model = lm(log(dat[, 1]) ~ dat[, 2] + dat[, 3] + dat[, 4] + dat[, 5] + dat[, 6] * dat[, 7])
   out[1] = summary(full_model)$r.squared
   out[8:14] = as.numeric(summary(full_model)$coef[2:8, 4])
-  T_model = lm(log(S) ~ mean_annual_T + PET, data = dat)
+  T_model = lm(log(dat[, 1]) ~ dat[, 2] + dat[, 3])
   out[2] = summary(T_model)$r.squared
-  no_T_model = lm(log(S) ~ AET + NDVI + alt_range * seasonality, data = dat)
+  no_T_model = lm(log(dat[, 1]) ~ dat[, 4] + dat[, 5] + dat[, 6] * dat[, 7])
   out[5] = summary(full_model)$r.squared - summary(no_T_model)$r.squared
-  prod_model = lm(log(S) ~ AET + NDVI, data = dat)
+  prod_model = lm(log(dat[, 1]) ~ dat[, 4] + dat[, 5])
   out[3] = summary(prod_model)$r.squared
-  no_prod_model = lm(log(S) ~ mean_annual_T + PET + alt_range * seasonality, data = dat)
+  no_prod_model = lm(log(dat[, 1]) ~ dat[, 2] + dat[, 3] + dat[, 6] * dat[, 7])
   out[6] = summary(full_model)$r.squared - summary(no_prod_model)$r.squared
-  Janzen_model = lm(log(S) ~ alt_range * seasonality, data = dat)
+  Janzen_model = lm(log(dat[, 1]) ~ dat[, 6] * dat[, 7])
   out[4] = summary(Janzen_model)$r.squared
-  no_Janzen_model = lm(log(S) ~ mean_annual_T + PET + AET + NDVI, data = dat)
+  no_Janzen_model = lm(log(dat[, 1]) ~ dat[, 2] + dat[, 3] + dat[, 4] + dat[, 5])
   out[7] = summary(full_model)$r.squared - summary(no_Janzen_model)$r.squared
   return(out)
 }
